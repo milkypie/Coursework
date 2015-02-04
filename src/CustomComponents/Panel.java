6286a8d9f1509f50f47e6ebbdd3ec31e90934a0d
James <James@192.168.0.253>
2015-02-04 13:43:04 +0000

    Added styles (like the buttons) to Panels

--- src/CustomComponents/Panel.java
+++ src/CustomComponents/Panel.java
@@ -3,7 +3,16 @@ package CustomComponents;
 import java.awt.Graphics;
 
 public class Panel extends GUIComponent{
+	public int Style = 0;
 	
+	public int getStyle() {
+		return Style;
+	}
+
+	public void setStyle(int style) {
+		Style = style;
+	}
+
 	public void setDimensions(int x,int y){
 		width = x;
 		height = y;
@@ -13,13 +22,16 @@ public class Panel extends GUIComponent{
 	@Override
 	public void Draw(Graphics g) {
 		g.setColor(Colour);
-		g.fillRect(Xpos, Ypos, width, height);
+		if(Style == 0){
+			g.fillRect(Xpos, Ypos, width, height);
+		}else if(Style == 1){
+			g.fillRoundRect(Xpos, Ypos, width, height, 10, 10);
+		}
 		
 	}
 
 	@Override
 	public void Update() {
-		// TODO Auto-generated method stub
 		
 	}
 
